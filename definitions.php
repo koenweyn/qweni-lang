5b66d2be22adc3a01136b6df784dd02c26878b99
Koen Weyn <koenweyn@blibo.be>
2014-12-14 17:50:55 +0100

    Initial commit

---
+++ definitions.php
@@ -0,0 +1,9 @@
+<?php
+
+ $LocaleInfo['qweni-lang'] = array (
+  'Locale' => 'nl-BE',
+  'Name' => 'Qweni / Nederlands',
+  'Description' => 'Adaptations for the qweni forum of the official Dutch language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/nl/">here</a>.',
+  'Version' => '1.0',
+  'Author' => 'kweyn'
+);
